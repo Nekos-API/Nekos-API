aafc98fbcc61a957374b755e59abb80db728797c
Neki <84998222+Nekidev@users.noreply.github.com>
2023-02-24 02:18:01 -0300

    Added SFW moderating guide

--- api/images/management/commands/upload.py
+++ api/images/management/commands/upload.py
@@ -5,6 +5,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from images.models import Image
 from users.models import User
+from artists.models import Artist
 
 class Command(BaseCommand):
 
@@ -12,6 +13,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('uploader', help="The uploader's username")
+        parser.add_argument('artist', help="The artist's name", default="")
 
     def handle(self, *args, **options):
         """
@@ -22,22 +24,26 @@ class Command(BaseCommand):
 
         i = 0
 
-        print(f"Uploading {total} images in the name of user {options['uploader']}...")
-
         uploader = User.objects.get(username=options['uploader'])
+        artist = Artist.objects.filter(name=options['artist']).first()
+
+        self.stdout.write(f"Uploading {total} images{'' if not artist else f' by {artist.name}'} in the name of user {uploader.username}...\n")
 
         for image in os.scandir('./upload/'):
             i += 1
 
             if Image.objects.filter(title=f"Uploaded by {uploader.username} - {image.name}"[:100]).exists():
-                print(f"Skipped - ({i}/{total})")
+                self.stdout.write(f"Skipped - ({i}/{total})")
                 continue
                 
             obj = Image.objects.create(
                 title=f"Uploaded by {uploader.username} - {image.name}"[:100],
                 uploader=uploader,
+                artist=artist,
                 verification_status=Image.VerificationStatus.NOT_REVIEWED,
                 file=File(open(image.path, 'rb'), image.name)
             )
 
             self.stdout.write(f"UPLOADED - {obj.id} - ({i}/{total})")
+        
+        self.stdout.write(f"\nSuccessfully uploaded {total} images.")
