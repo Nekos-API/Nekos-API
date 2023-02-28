4869077d1faf531c149931483ce8fc7fca0d9fe2
Neki <84998222+Nekidev@users.noreply.github.com>
2023-02-28 00:28:39 -0300

    Moved admin to admin subdomain root

--- api/nekos_api/urls.py
+++ api/nekos_api/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("api.urls")),
+] if settings.DEBUG else [
+    path("/", admin.site.urls),
 ]
