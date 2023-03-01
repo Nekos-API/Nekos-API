1381412c6d39313230abed0f6d4eed8f50043c81
Neki <84998222+Nekidev@users.noreply.github.com>
2023-02-28 23:45:38 -0300

    Added age rating content moderation guide

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -48,10 +48,10 @@ export default {
         "link": "https://discord.gg/b9Fv3kEfXc"
     },
     banner: {
-        "key": "v2-almost-finished",
+        "key": "v2-alpha-live",
         "text": (
-            <Link href="https://github.com/Nekos-API/Nekos-API" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: ".5rem" }}>
-                ✨ <span className="font-mono">v2.0.0-alpha</span> is almost finished! You can check out the current progress at it's GitHub repository ✨
+            <Link href="/docs" style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center", gap: ".5rem" }}>
+                ✨ <span className="font-mono">v2.0.0-alpha</span> is live! You can check the documentation here ✨
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" style={{ height: "1rem", width: "1rem" }}>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                 </svg>
