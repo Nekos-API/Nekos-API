1373fcea75070d5d6b88a3a6f6f2cd5bbaad2e25
Neki <84998222+Nekidev@users.noreply.github.com>
2023-03-06 22:22:26 -0300

    Fixed admin base url bug

--- api/nekos_api/urls.py
+++ api/nekos_api/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("api.urls")),
 ] if settings.DEBUG else [
-    path("/", admin.site.urls),
+    path("", admin.site.urls),
 ]
