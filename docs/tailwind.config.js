11112fb0964ab1d911ed0774cc6aeedc86651c15
Neki <84998222+Nekidev@users.noreply.github.com>
2023-03-15 01:11:06 -0300

    Added `theme.config.jsx` to tailwind sources

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./theme.config.jsx"
   ],
   darkMode: 'class',
   theme: {
