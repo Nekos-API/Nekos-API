b736665ed0c87323f09b53239497d3d12a68af58
Neki <84998222+Nekidev@users.noreply.github.com>
2023-03-31 20:09:53 -0300

    Updated `colors` command

--- api/images/management/commands/colors.py
+++ api/images/management/commands/colors.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
         """
         Sets the color palette of an image.
         """
-        images = Image.objects.filter(primary_color__isnull=True, verification_status="verified").exclude(Q(file="") | Q(file__isnull=True))
+        images = Image.objects.filter(Q(dominant_color__isnull=True) | Q(palette__isnull=True), verification_status="verified").exclude(Q(file="") | Q(file__isnull=True))
         total_images = images.count()
 
         j = 1
@@ -23,8 +23,10 @@ class Command(BaseCommand):
         for image in images:
             ct = ColorThief(io.BytesIO(requests.get(image.file.url).content))
             dominant = ct.get_color(quality=1)
+            palette = ct.get_palette(color_count=10, quality=1)
 
             image.dominant_color = list(dominant)
+            image.palette = list(palette)
             image.save()
 
             self.stdout.write(self.style.SUCCESS("SUCCESS") + f" - {image.id} - {image.dominant_color} - ({j}/{total_images})")
