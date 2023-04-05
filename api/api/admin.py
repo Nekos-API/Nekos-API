21ada1f4ff376a5da7ba9533a6fabcd87607680f
Neki <84998222+Nekidev@users.noreply.github.com>
2023-04-05 01:33:36 -0300

    Updated logs admin panel

--- api/api/admin.py
+++ api/api/admin.py
@@ -20,18 +20,6 @@ class LogEntryAdmin(admin.ModelAdmin):
         "action_flag",
     ]
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_view_permission(self, request, obj=None):
-        return request.user.is_superuser
-
     def object_link(self, obj):
         if obj.action_flag == DELETION:
             link = escape(obj.object_repr)
