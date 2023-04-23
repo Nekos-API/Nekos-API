b41830901413350b9741220492687f32c796c18d
Neki <84998222+Nekidev@users.noreply.github.com>
2023-04-23 16:38:34 -0300

    Updated static files root URL

--- api/nekos_api/settings.py
+++ api/nekos_api/settings.py
@@ -281,7 +281,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "https://cdn.nekosapi.com/"
 STATIC_ROOT = "/"
 
 MEDIA_URL = "https://cdn.nekosapi.com/"
