9c7de59ac25e5407c8308426f40118b0575bdb26
Neki <84998222+Nekidev@users.noreply.github.com>
2023-04-23 16:24:25 -0300

    Updated static files host to CDN

--- api/nekos_api/settings.py
+++ api/nekos_api/settings.py
@@ -190,6 +190,8 @@ CACHES = {
     }
 }
 
+STORAGES = {"staticfiles": {"BACKEND": "nekos_api.storage.Storage"}}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
