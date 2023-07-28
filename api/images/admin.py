6a75a14379075a5548a041c2d56e1ff66c30c0a8
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-07-28 00:44:42 -0300

    Merged duplicated `readonly_fields` in image admin

--- api/images/admin.py
+++ api/images/admin.py
@@ -5,11 +5,12 @@ from .models import Image, ImageSourceResult
 # Register your models here.
 
 
-@admin.action(description='Mark selected images as verified')
+@admin.action(description="Mark selected images as verified")
 def verify_images(modeladmin, request, queryset):
     queryset.update(verification_status=Image.VerificationStatus.VERIFIED)
 
-@admin.action(description='Mark selected images as not reviewed')
+
+@admin.action(description="Mark selected images as not reviewed")
 def unverify_images(modeladmin, request, queryset):
     queryset.update(verification_status=Image.VerificationStatus.NOT_REVIEWED)
 
@@ -19,21 +20,37 @@ class ImageAdmin(admin.ModelAdmin):
     Images admin.
     """
 
-    list_display = ('title', 'artist', 'age_rating', 'verification_status', 'source_name', 'created_at', 'updated_at')
-    list_filter = ('is_original', 'verification_status', 'age_rating')
-    raw_id_fields = ('artist',)
-    filter_horizontal = ('characters', 'categories')
-    readonly_fields = ('height', 'width', 'aspect_ratio', 'palette', 'dominant_color', 'mimetype', 'file_size')
-    search_fields = ('title', 'uploader__username', 'artist__name', 'artist__aliases')
-    autocomplete_fields = ('characters', 'categories')
-    readonly_fields = ("hash_perceptual",)
+    list_display = (
+        "title",
+        "artist",
+        "age_rating",
+        "verification_status",
+        "source_name",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("is_original", "verification_status", "age_rating")
+    raw_id_fields = ("artist",)
+    filter_horizontal = ("characters", "categories")
+    readonly_fields = (
+        "height",
+        "width",
+        "aspect_ratio",
+        "palette",
+        "dominant_color",
+        "mimetype",
+        "file_size",
+        "hash_perceptual",
+    )
+    search_fields = ("title", "uploader__username", "artist__name", "artist__aliases", "categories__name")
+    autocomplete_fields = ("characters", "categories")
     actions = [verify_images, unverify_images]
 
 
 class ImageSourceResultAdmin(admin.ModelAdmin):
-    list_display = ('id', 'image', 'similarity', 'status')
-    list_filter = ('status',)
-    raw_id_fields = ('image',)
+    list_display = ("id", "image", "similarity", "status")
+    list_filter = ("status",)
+    raw_id_fields = ("image",)
 
 
 admin.site.register(Image, ImageAdmin)
