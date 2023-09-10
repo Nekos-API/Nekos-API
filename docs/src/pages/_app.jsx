7e0bbee40b78400b7123b7315975c0b8236391d3
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-09-10 01:16:56 -0300

    Fixed page overflow in medium-sized screens

--- docs/src/pages/_app.jsx
+++ docs/src/pages/_app.jsx
@@ -21,13 +21,17 @@ const asap = Rubik({ subsets: ["latin"] });
 
 const Contributor = ({ username, image, url }) => {
     return (
-        <Link href={url} className="relative flex flex-col items-center" target="_blank">
+        <Link href={url} className="relative flex flex-col items-center shrink-0" target="_blank">
             <img src={image} loading="lazy" className="h-7 w-7 object-cover block hover:scale-110 transition-all rounded-full peer" />
             <div className="absolute bottom-full bg-white dark:bg-neutral-900 border border-neutral-200 dark:border-neutral-800 drop-shadow-md rounded mx-auto whitespace-nowrap flex flex-col items-center py-1 px-2 transition-all opacity-0 mb-0 peer-hover:opacity-100 peer-hover:mb-2 text-neutral-900 dark:text-[#f1f5f9] pointer-events-none after:absolute after:-bottom-1 after:left-0 after:right-0 after:mx-auto after:w-2 after:h-2 after:rotate-45 after:rounded-sm after:bg-white after:dark:bg-neutral-900 after:border after:border-neutral-200 after:dark:border-neutral-800 after:!border-t-transparent after:!border-l-transparent">
                 <span className="text-xs">{username}</span>
             </div>
         </Link>
     )
+}
+
+const FooterLink = ({ href, children, ...props }) => {
+    return <Link className="hover:text-neutral-800 dark:hover:text-neutral-200 shrink-1 grow-1" href={href} {...props}>{children}</Link>
 }
 
 export default function NekosAPI({ Component, pageProps }) {
@@ -37,20 +41,21 @@ export default function NekosAPI({ Component, pageProps }) {
             <hr className="dark:border-neutral-800" />
             <div
                 className={
-                    'mx-auto flex flex-col-reverse lg:flex-row w-full max-w-[90rem] lg:items-center justify-between py-2 text-gray-600 dark:text-gray-400 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)] bg-gray-100 dark:bg-neutral-900 print:bg-transparent text-sm gap-8'
+                    'mx-auto flex flex-col-reverse md:grid md:grid-cols-[1fr_min-content] xl:flex xl:flex-row w-full max-w-[90rem] xl:items-center justify-between py-2 text-gray-600 dark:text-gray-400 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)] bg-gray-100 dark:bg-neutral-900 print:bg-transparent text-sm gap-8 md:gap-16 relative'
                 }
             >
-                <div className='flex flex-row items-center justify-between gap-x-8 gap-y-2 flex-wrap'>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://nyeki.dev/">Nyeki.py</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://nekos.land/">Nekos.Land</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://nekosauce.org/">NekoSauce</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://github.com/Nekos-API/Nekos-API">GitHub</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://status.nekosapi.com/">Status</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="https://discord.gg/PgQnuM3YnM">Discord</Link>
-                    <Link className="hover:text-neutral-800 dark:hover:text-neutral-200" href="/donate">Donate</Link>
+                <div className='flex flex-row items-center justify-between md:justify-start gap-x-8 gap-y-2 flex-wrap'>
+                    <FooterLink href="https://nyeki.dev/" target="_blank">Nyeki.py</FooterLink>
+                    <FooterLink href="https://nekos.land/" target="_blank">Nekos.Land</FooterLink>
+                    <FooterLink href="https://nekosauce.org/" target="_blank">NekoSauce</FooterLink>
+                    <FooterLink href="https://github.com/Nekos-API/Nekos-API">GitHub Repo</FooterLink>
+                    <FooterLink href="https://status.nekosapi.com/">Status</FooterLink>
+                    <FooterLink href="https://discord.gg/PgQnuM3YnM">Discord</FooterLink>
+                    <FooterLink href="/donate">Donate</FooterLink>
+                    <FooterLink href="mailto:dmca@nekosapi.com">DMCA</FooterLink>
                 </div>
-                <div className='flex flex-row items-center gap-2 justify-between sm:justify-start'>
-                    <span className="hidden sm:inline-block">Thanks to:</span>
+                <div className='flex flex-row items-center gap-2 justify-between md:justify-start md:ml-auto min-w-max'>
+                    <span className="hidden md:inline-block whitespace-nowrap">Thanks to:</span>
                     {contributors.map((contributor) => (
                         <Contributor
                             key={contributor.id}
