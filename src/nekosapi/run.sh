05870b0463353962af00cc7d70bf15ddc7e96082
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-22 23:38:55 -0300

    Reorder run.sh commands

--- src/nekosapi/run.sh
+++ src/nekosapi/run.sh
@@ -2,5 +2,5 @@ cd /app
 
 uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers ${API_WORKERS:-4} &
 
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py collectstatic --no-input
