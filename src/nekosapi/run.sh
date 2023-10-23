667b77b36eb0dff969ae1cb60a998dade8f672c5
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-22 22:41:33 -0300

    Set default API workers

--- src/nekosapi/run.sh
+++ src/nekosapi/run.sh
@@ -1,3 +1,3 @@
 python manage.py collectstatic --no-input
 python manage.py migrate
-uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers $(API_WORKERS)
+uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers ${API_WORKERS:-4}
