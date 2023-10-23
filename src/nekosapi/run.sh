5493ddcdcbe7edefa4c56ab9d173f2ea5355cd49
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-22 23:27:16 -0300

    Move uvicorn workers to the top

--- src/nekosapi/run.sh
+++ src/nekosapi/run.sh
@@ -1,5 +1,6 @@
 cd /app
 
+uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers ${API_WORKERS:-4} &
+
 python manage.py collectstatic --no-input
 python manage.py migrate
-uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers ${API_WORKERS:-4}
