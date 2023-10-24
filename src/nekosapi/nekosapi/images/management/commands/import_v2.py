8e7e81c7b4a1ff4c15187e31b3305a8226cea6fb
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-10-23 23:32:00 -0300

    Patch import_v2

--- src/nekosapi/nekosapi/images/management/commands/import_v2.py
+++ src/nekosapi/nekosapi/images/management/commands/import_v2.py
@@ -1,5 +1,6 @@
-from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
 from django.core.files.base import ContentFile
+from django.core.management.base import BaseCommand
 
 import requests
 
@@ -35,8 +36,12 @@ class Command(BaseCommand):
                     else image["attributes"]["ageRating"],
                 )
                 im.image.save("image.webp", ContentFile(requests.get(image["attributes"]["file"]).content), save=True)
-                im.process()
-                self.stdout.write(f"Image {im.id} imported")
+                try:
+                    im.process()
+                    self.stdout.write(f"Image {im.id} imported")
+                except IntegrityError:
+                    im.delete()
+
                 del im
 
                 i += 1
