fbe3bcdf223deb3324b62679d847d16f1146de63
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2023-11-02 15:54:24 -0300

    Patch `/api/status`

--- src/docs/src/pages/api/status.js
+++ src/docs/src/pages/api/status.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const r = await fetch('https://betteruptime.com/api/v2/monitors/1100535', {
+    const r = await fetch(`https://betteruptime.com/api/v2/monitors/${process.env.BETTER_UPTIME_MONITOR_ID}`, {
         headers: {
             Authorization: `Bearer ${process.env.BETTER_UPTIME_TOKEN}`,
             Accept: `application/vnd.api+json`
