d94543dfdffcccf528730ac5a7160b5c43cac02e
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2024-07-16 00:11:06 -0300

    feat(api): :coffin: Delete v3 API files.

--- src/api/nekosapi/parsers.py
+++
@@ -1,8 +0,0 @@
-from ninja.parser import Parser
-
-import orjson
-
-
-class ORJSONParser(Parser):
-    def parse_body(self, request):
-        return orjson.loads(request.body)
--- src/api/run.sh
+++
@@ -1,6 +0,0 @@
-cd /app
-
-uvicorn nekosapi.asgi:application --host 0.0.0.0 --port 8000 --workers ${API_WORKERS:-4} &
-
-python manage.py migrate
-python manage.py collectstatic --no-input
