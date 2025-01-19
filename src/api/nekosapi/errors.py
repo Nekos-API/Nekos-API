b82b2420e271380b98f5d79051f9f697fa181672
Nyeki.py <84998222+Nekidev@users.noreply.github.com>
2025-01-18 22:56:19 -0300

    docs: :memo: Update changelog.

--- src/api/nekosapi/errors.py
+++
@@ -1,5 +0,0 @@
-class HttpError(Exception):
-    def __init__(self, status_code: int = 500, errors: list = []) -> None:
-        self.errors = errors
-        self.status_code = status_code
-        super().__init__()
--- src/api/nekosapi/images/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ImagesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "nekosapi.images"
