fd46f52fee6cede2568e36d93a65d1bd22984586
Rafael Bradley <84998222+Nekidev@users.noreply.github.com>
2025-04-01 14:30:59 -0300

    feat: Add CORS headers to API endpoints.

--- src/docs/next.config.js
+++ src/docs/next.config.js
@@ -23,6 +23,15 @@ module.exports = withNextra({
                     },
                 ],
             },
+            {
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
         ];
     },
 });
